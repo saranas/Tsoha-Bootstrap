eba532c9edd34996961b64ffaa147bdbc26673e2
Saara Akkanen <saara.akkanen@gmail.com>
2015-08-01 13:15:17 +0300

    sivunäkymiä työstetty

--- app/controllers/hello_world_controller.php
+++ app/controllers/hello_world_controller.php
@@ -1,14 +1,25 @@
 <?php
 
   class HelloWorldController extends BaseController{
+      
+    public static function drinks_list() {
+        View::make('suunnitelmat/drinks_list.html');
+    }
+    
+    public static function drink_show() {
+        View::make('suunnitelmat/drink_show.html');
+    }
 
     public static function index(){
       // make-metodi renderöi app/views-kansiossa sijaitsevia tiedostoja
-   	  View::make('home.html');
+   	  //View::make('home.html');
+        echo 'Tämä on etusivu!';
     }
+    
 
     public static function sandbox(){
       // Testaa koodiasi täällä
-      echo 'Hello World!';
+      //echo 'Hello World!';
+        View::make('helloworld.html');
     }
   }
--- config/routes.php
+++ config/routes.php
@@ -3,6 +3,14 @@
   $routes->get('/', function() {
     HelloWorldController::index();
   });
+  
+  $routes->get('/drinks_list', function() {
+    HelloWorldController::drinks_list();
+  });
+  
+  $routes->get('/drink_show', function() {
+    HelloWorldController::drink_show();
+  });
 
   $routes->get('/hiekkalaatikko', function() {
     HelloWorldController::sandbox();
