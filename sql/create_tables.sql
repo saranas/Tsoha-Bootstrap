6591630a481fe3948d68076d0cd2d9eec3c55242
Saara Akkanen <saara.akkanen@gmail.com>
2015-08-02 18:31:40 +0300

    create tables

--- sql/create_tables.sql
+++ sql/create_tables.sql
@@ -1,1 +1,30 @@
--- Lisää CREATE TABLE lauseet tähän tiedostoon
+
+CREATE TABLE Aines(
+    aines_id SERIAL PRIMARY KEY, --aineksen uniikki tunniste
+    nimi varchar(50)NOT NULL, --aineksen nimi, esim. ananasmehu, Midori Melon
+    alkpitoisuus decimal --aineksen alkoholipitoisuus prosentteina
+);
+
+CREATE TABLE Lasi(
+    lasi_id SERIAL PRIMARY KEY,
+    nimi varchar(50) --lasin nimi
+);
+
+CREATE TABLE Drinkki(
+    drinkki_id SERIAL PRIMARY KEY, --Drinkin uniikki tunniste
+    nimi varchar(50) NOT NULL, --drinkin nimi
+    tyyppi varchar(30), --shotti, cocktail tms
+    alkoholiton boolean, --onko juoma täysin alkoholiton
+    lasi INTEGER REFERENCES Lasi(lasi_id) --käytettävä lasi
+);
+
+CREATE TABLE Drinkkiainekset(
+    drinkki_id INTEGER REFERENCES Drinkki(drinkki_id) PRIMARY KEY,
+    aines_id INTEGER REFERENCES Aines(aines_id) PRIMARY KEY
+);
+
+CREATE TABLE Kayttaja(
+    kayttaja_id SERIAL PRIMARY KEY, --id
+    kayttajanimi varchar(20), --
+    salasana varchar(20)
+);
