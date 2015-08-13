adc20128ddd518fb5e35c5c5b677453c69b688fb
Saara Akkanen <saara.akkanen@gmail.com>
2015-08-13 11:27:58 +0300

    lasi-taulu poistettu ja muutettu drinkin attribuutiksi

--- sql/add_test_data.sql
+++ sql/add_test_data.sql
@@ -3,16 +3,12 @@
 INSERT INTO Aines (nimi, alkpitoisuus) VALUES ('Ananasmehu', '0.0');
 INSERT INTO Aines (nimi, alkpitoisuus) VALUES ('Original Lime', '4.5');
 
---Lasi-taulun testidata
-INSERT INTO Lasi (nimi) VALUES ('Cocktail-lasi');
-INSERT INTO Lasi (nimi) VALUES ('Hurrikaanilasi');
-
 --Drinkki-taulun testidata
 INSERT INTO Drinkki (nimi, tyyppi, alkoholiton, kuvaus, lasi) VALUES ('Testidrinkki', 'Cocktail', false, 'Testidrinkki on herkullinen alkoholiton versio Pina Coladasta. Drinkin voi 
     valmistaa myös kaupasta saatavasta pina colada -siirapista, mutta tuoreilla 
-    aineksilla lopputulos on paljon maukkaampi.', 1);
+    aineksilla lopputulos on paljon maukkaampi.', 'Hurrikaanilasi');
 INSERT INTO Drinkki (nimi, tyyppi, alkoholiton, kuvaus, lasi) VALUES ('Mehumehu', 'Smoothie', true, 'Mehumehu on herkullinen alkoholiton versio Pina Coladasta. Drinkin voi 
-    valmistaa myös kaupasta saatavasta pina colada -siirapista.', 1);
+    valmistaa myös kaupasta saatavasta pina colada -siirapista.', 'Cocktail-lasi');
 
 --Drinkkiainekset-taulun testidata
 INSERT INTO Drinkkiainekset (drinkki_id, aines_id) VALUES (1, 1);
--- sql/create_tables.sql
+++ sql/create_tables.sql
@@ -5,18 +5,13 @@ CREATE TABLE Aines(
     alkpitoisuus decimal --aineksen alkoholipitoisuus prosentteina
 );
 
-CREATE TABLE Lasi(
-    lasi_id SERIAL PRIMARY KEY,
-    nimi varchar(50) --lasin nimi
-);
-
 CREATE TABLE Drinkki(
     drinkki_id SERIAL PRIMARY KEY, --Drinkin uniikki tunniste
     nimi varchar(50) NOT NULL, --drinkin nimi
     tyyppi varchar(30), --shotti, cocktail tms
     alkoholiton boolean, --onko juoma täysin alkoholiton
     kuvaus text, --selitysteksti juomalle
-    lasi INTEGER REFERENCES Lasi(lasi_id) --käytettävä lasi
+    lasi varchar(30) --käytettävä lasi
 );
 
 CREATE TABLE Drinkkiainekset(
